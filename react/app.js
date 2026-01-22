c5fc8e2d02ce5e9cc8fd76b4b592bb851c58c911
Mohamed Farhaan <157977904+mdfarhaan13@users.noreply.github.com>
2026-01-22 10:48:37 +0530

    Create app.js

---
+++ react/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h2>Missing Person Finder</h2>
+      <button>Upload Image</button>
+      <button>Search Database</button>
+    </div>
+  );
+}
+
+export default App;
